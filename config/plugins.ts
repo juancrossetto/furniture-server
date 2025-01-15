6ca3afe9050839bd17e7438eff615d824694b6d4
Juanma Crossetto <juancrossetto@gmail.com>
2025-01-15 09:48:05 -0300

    feat: se instala postgress y se configura supabase

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+// export default () => ({});
+
+
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: "supabase",
+        providerOptions: {
+          apiUrl: env("SUPABASE_API_URL", "https://your-supabase-url.supabase.co"),
+          apiKey: env("SUPABASE_API_KEY", "your-supabase-api-key"),
+          bucket: env("SUPABASE_BUCKET", "strapi-uploads"),
+          directory: env("SUPABASE_DIRECTORY", "uploads"),
+          options: {},
+        },
+      },
+    },
+  });
+  
