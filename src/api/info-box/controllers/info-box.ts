280d2a4a6dcdb774e08540fa7e193e23be151a1a
Juanma Crossetto <juancrossetto@gmail.com>
2025-01-15 09:18:26 -0300

    first commit}

---
+++ src/api/info-box/controllers/info-box.ts
@@ -0,0 +1,7 @@
+/**
+ * info-box controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::info-box.info-box');
---
+++ src/api/info-box/services/info-box.ts
@@ -0,0 +1,7 @@
+/**
+ * info-box service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::info-box.info-box');
