94dce8a525faee06e455da38ebaecbcc2f115b5a
Juanma Crossetto <juancrossetto@gmail.com>
2025-01-15 11:28:21 -0300

    feat: mas configuracion para supabase

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,9 +4,9 @@
 export default ({ env }) => ({
     upload: {
       config: {
-        provider: "supabase",
+        provider: "strapi-provider-upload-supabase",
         providerOptions: {
-          apiUrl: env("SUPABASE_API_URL", "https://your-supabase-url.supabase.co"),
+          apiUrl: env("SUPABASE_API_URL", "https://icspknzugbnnanbidolg.supabase.co"),
           apiKey: env("SUPABASE_API_KEY", "your-supabase-api-key"),
           bucket: env("SUPABASE_BUCKET", "strapi-uploads"),
           directory: env("SUPABASE_DIRECTORY", "uploads"),
